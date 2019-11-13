4516a224bf2946aa18c0a3f9f2dd94cd7f8fec9e
ricelp <ricelp@hotmail.com>
2019-11-13 15:09:09 -0500

    primer
    inicio proyecto

---
+++ sales2019.Common/Models/Equipo.cs
@@ -0,0 +1,45 @@
+﻿
+namespace sales2019.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Equipo
+    {
+        [Key]
+        public int codinfo { get; set; }
+        [Key]
+        public int codequipo { get; set; }
+        [Required]
+        public string xnombre { get; set; }
+        public string xdescripcion { get; set; }
+        public Nullable<int> cmarca { get; set; }
+        public string xmarca { get; set; }
+        public Nullable<int> cmodelo { get; set; }
+        public string xmodelo { get; set; }
+        public Nullable<int> cmarcamotor { get; set; }
+        public string xmarcamotor { get; set; }
+        public string xserialChasis { get; set; }
+        public string xserialMotor { get; set; }
+        public string xplaca { get; set; }
+        public Nullable<int> aa { get; set; }
+        public string itipoequipo { get; set; }
+        public Nullable<System.DateTime> fpesosydim { get; set; }
+        public Nullable<System.DateTime> fseguro { get; set; }
+        public Nullable<System.DateTime> ffrenos { get; set; }
+        public Nullable<System.DateTime> fplaca { get; set; }
+        public string icambioaceite { get; set; }
+        public Nullable<System.DateTime> fproximoCA { get; set; }
+        public string imantenimiento { get; set; }
+        public Nullable<System.DateTime> fproximoMA { get; set; }
+        public string iengrasado { get; set; }
+        public Nullable<System.DateTime> fproximoEN { get; set; }
+        public Nullable<int> cantcombustible { get; set; }
+        public Nullable<int> cantcombustiblebarre { get; set; }
+        public Nullable<int> consumodiesel_km { get; set; }
+        public Nullable<int> consumodiesel_h { get; set; }
+        public decimal km { get; set; }
+        public decimal odm { get; set; }
+        public string xobservacion { get; set; }
+        public string iestado { get; set; }
+    }
+}
--- sales2019/sales2019/ViewModels/ProductsViewModel.cs
+++ sales2019/sales2019/ViewModels/ProductsViewModel.cs
@@ -3,19 +3,28 @@ namespace sales2019.ViewModels
 {
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
     using sales2019.Services;
     using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
         private ApiService apiService;
-        
+        public bool isRefreshing;
+
         public ObservableCollection<Product> products;
         public ObservableCollection<Product> Products
         {
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
         }
 
         public ProductsViewModel()
@@ -26,16 +35,26 @@ namespace sales2019.ViewModels
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
             var response = await this.apiService.GetList<Product>("http://www.simgsaapi.somee.com", "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
 
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+        }
 
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
         }
     }
 }
